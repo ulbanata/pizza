728eac88c03d5925c1e91e8a8b0f9d8f4dd55aa6
Patrick Brennan <chief125@gmail.com>
2014-02-10 11:05:57 -0600

    Add a failing test for Topping.initialize.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -10,4 +10,10 @@ describe Topping do
   it "exists" do
     expect(Topping).to be_a(Class)
   end
+  describe '.initialize' do
+    it "sets the name of the topping" do
+
+    expect(toppy.name).to eq('olives')
+    end
+  end
 end
