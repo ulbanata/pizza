a7659fbc9e3b2a23031d809b6429ec03651969bb
Patrick Brennan <chief125@gmail.com>
2014-02-10 11:13:53 -0600

    Added a failing test for vegetarian toppings.

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,9 @@ class Pizza
 end
 
 class Topping
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -12,8 +12,13 @@ describe Topping do
   end
   describe '.initialize' do
     it "sets the name of the topping" do
+      topping = Topping.new('olives')
+      expect(topping.name).to eq('olives')
+    end
+    it "sets whether or not the topping is vegetarian" do
+      topping = Topping.new 'bell peppers', vegetarian: true
 
-    expect(toppy.name).to eq('olives')
+      expect(topping.vegetarain).to eq(true)
     end
   end
 end
